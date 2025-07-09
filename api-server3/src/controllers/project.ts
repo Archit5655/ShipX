0f5c2ea350e1c914194dc97cb014dab79ae12fc3
Archit5655 <architgargagra@gmail.com>
2025-07-09 05:30:46 +0530

    Refactor ECS client and command handling for improved project creation

--- api-server3/src/controllers/project.ts
+++ api-server3/src/controllers/project.ts
@@ -1,27 +1,23 @@
 import type { Context } from "hono";
-import ecsClient from "../services/ecs.client.js";
+// import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
+import { ecsClient, ContainerSpinCommnad } from "../services/ecs.client.js";
+import { generateSlug } from "random-word-slugs";
+
 export const createProject = async (c: Context) => {
   const body = await c.req.json();
+  const projectSlug = generateSlug();
   const { GitRepoUrl } = body;
-  if (
-    !GitRepoUrl ||
-    typeof GitRepoUrl !== "string" ||
-    GitRepoUrl.trim() === ""
-  ) {
-    return c.json(
-      {
-        status: "error",
-        message: "gitURL is required and must be a valid string",
-      },
-      400
+  // prettier-ignore
+  if (!GitRepoUrl ||typeof GitRepoUrl !== "string" ||GitRepoUrl.trim() === "") {
+    return c.json({
+      status: "error",
+      message: "gitURL is required and must be a valid string",
+      },400
     );
   }
+
+
   
-
-
-
-
-
-
-
+  const command = ContainerSpinCommnad(GitRepoUrl, projectSlug);
+  await ecsClient.send(command);
 };
--- api-server3/src/services/ecs.client.ts
+++ api-server3/src/services/ecs.client.ts
@@ -2,14 +2,42 @@ import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
 import { config } from "../config/config.js";
 
 const region = config.AWS.REGION;
-const accessKey =config.AWS.ACCESS_KEY; ;
+const accessKey = config.AWS.ACCESS_KEY;
 const secretKey = config.AWS.SECRET_ACCESS_KEY;
 
-const ecsClient = new ECSClient({
+export const ecsClient = new ECSClient({
   region: region!,
   credentials: {
     accessKeyId: accessKey!,
     secretAccessKey: secretKey!,
   },
 });
-export default ecsClient;
+
+export const ContainerSpinCommnad = (GitRepoUrl: string, projectID: string) => {
+  return new RunTaskCommand({
+    cluster: config.ECS.CLUSTER,
+    taskDefinition: config.ECS.TASK,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        subnets: config.AWS.SUBNETS,
+        securityGroups: config.AWS.SECURITY_GROUPS,
+        assignPublicIp: "ENABLED",
+      },
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: "builder-image",
+          environment: [
+            { name: "GIT_REPOSITORY__URL", value: GitRepoUrl },
+            { name: "PROJECT_ID", value: projectID },
+          ],
+        },
+      ],
+    },
+  });
+};
+
+
