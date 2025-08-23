0765f53b7f910eb899d945af4cf1fcaa9be7f39c
Archit5655 <architgargagra@gmail.com>
2025-08-24 00:07:52 +0530

    BUTOTON

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col relative items-center justify-center h-screen bg-yellow-500 overflow-hidden">
         <div className="w-80 h-80 rounded-full absolute bottom-[-100px] bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
         Hello
-        {/* <Button /> */}
+        <Button />
       </div>
     </>
   );
