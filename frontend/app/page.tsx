fddcabf379e2c5ed9c3d5898439371a2ac6718f1
Archit5655 <architgargagra@gmail.com>
2025-08-23 06:28:36 +0530

    gradient

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,11 +3,9 @@ import Button from "@/components/Button";
 export default function Home() {
   return (
     <>
-      <div className=" flex flex-col items-center justify-center min-h-screen bg-yellow-500">
-
-<div className="w-80  absolute bottom-[-200px] h-80 rounded-full bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
+      <div className="flex flex-col relative items-center justify-center h-screen bg-yellow-500 overflow-hidden">
+        <div className="w-80 h-80 rounded-full absolute bottom-[-100px] bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
         Hello
-
         {/* <Button /> */}
       </div>
     </>
