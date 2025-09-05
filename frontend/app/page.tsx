f7c7f9def69c7a1d21046ea404e21becdd1c0929
Archit5655 <architgargagra@gmail.com>
2025-09-05 23:57:16 +0530

    chnage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="flex flex-col relative items-center  justify-center h-screen  overflow-hidden bg-black ">
         {/* <div className="w-80 h-80 rounded-full absolute bottom-[-100px] bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
         Hello */}
-        <Button />
+        {/* <Button /> */}
         <Image src={"Ellipse361.svg"} alt="image" width="400" height={400} className=" absolute bottom-0 right-0  blur-[170px]"  />
 
       </div>
