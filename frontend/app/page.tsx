827532e1d3958ed33528df88c6c6268c1fe12631
Archit5655 <architgargagra@gmail.com>
2025-09-16 01:00:40 +0530

    added backgorund

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,13 +4,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col relative items-center  justify-center h-screen  overflow-hidden bg-black ">
-        {/* <div className="w-80 h-80 rounded-full absolute bottom-[-100px] bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
+      {/* <div classNameName="flex flex-col relative items-center  justify-center h-screen  overflow-hidden bg-black ">
+        {/* <div classNameName="w-80 h-80 rounded-full absolute bottom-[-100px] bg-[radial-gradient(circle,rgba(38,139,221,0.8)_0%,rgba(38,139,221,0)_100%)]" />
         Hello */}
-        {/* <Button /> */}
-        <Image src={"Ellipse361.svg"} alt="image" width="400" height={400} className=" absolute bottom-0 right-0  blur-[170px]"  />
+        {/* <Button />
+        <Image src={"Ellipse361.svg"} alt="image" width="400" height={400} className=" absolute bottom-0 right-0  blur-[170px]"  /> */}
 
-      </div>
+      {/* </div> */}
+  <div className="min-h-screen bg-[url('/grid3.png')]  bg-[length:100px_100px]">
+  <div className="flex items-center justify-center min-h-screen">
+    <h1 className="text-4xl font-bold text-gray-800 bg-white/80 p-6 rounded-xl shadow-lg">
+      ShipX Home Page
+    </h1>
+  </div>
+</div>
+
     </>
   );
 }
